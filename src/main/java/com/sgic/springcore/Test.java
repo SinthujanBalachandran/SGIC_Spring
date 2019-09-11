39d259d8fd0324bfe045d8789a26aabe0a6727f7
SinthujanBalachandran <sinthujan2233@gmail.com>
2019-09-11 21:11:33 +1000

    11.9

---
+++ src/main/java/com/sgic/springcore/Test.java
@@ -0,0 +1,14 @@
+package com.sgic.springcore;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	public static void main(String args[]) {
+		
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
+		Employee emp = (Employee) ctx.getBean("emp");
+		System.out.println("Employee ID:"+emp.getId());
+		System.out.println("Employee Name:"+emp.getName());
+	}
+
+}
